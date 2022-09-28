82e4b68ea3b585cd62ce13f507e967a9b013b57b
DELL <DELL@DESKTOP-LILEBQG>
2022-09-28 14:30:22 +0530

    Third commit

--- src/main/java/godmadhugitpractise/Sample.java
+++ src/main/java/godmadhugitpractise/Sample.java
@@ -5,5 +5,6 @@ public class Sample
 public static void main(String[] args) {
 	System.out.println("my teacher is madhu");
 	System.out.println("i am doing changes");
+	System.out.println(" again i am ding changes");
 }
 }
